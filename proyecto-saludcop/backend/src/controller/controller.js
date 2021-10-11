f9eb975e7b55917212461f07d94aa47b2f2fe1cb
JoseProgramm <92260002+JoseProgramm@users.noreply.github.com>
2021-10-11 17:03:08 -0400

    Delete proyecto-saludcop/backend directory

--- proyecto-saludcop/backend/src/controller/controller.js
+++
@@ -1,2 +0,0 @@
-const Pacientes = require('../models/Pacientes');
-exports.registroPacienteController = async (req, res) => {};
--- proyecto-saludcop/backend/src/router/index.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require('express');
-const { registroPacienteController } = require('../controller/controller');
-const router = Router();
-
-router.post('/api/registro-paciente', registroPacienteController);
-module.exports = router;
